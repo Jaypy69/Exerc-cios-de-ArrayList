ed3f31a3b0172f6022d1d86b7750f74a85a056f5
João Pedro <joaopedroalvez63@gmail.com>
2024-07-24 15:49:37 -0300

    Exercícios de arraylist

---
+++ Lpaaula57.java
@@ -0,0 +1,23 @@
+package LPAlore;
+import java.util.ArrayList;
+import java.util.Random;
+public class Lpaaula57 {
+
+	public static void main(String[] args) {
+		ArrayList<Integer> numero = new ArrayList<>();
+		numero.add(1);
+		numero.add(2);
+		numero.add(3);
+		numero.add(4);
+		numero.add(5);
+		numero.add(6);
+		numero.add(7);
+		numero.add(8);
+		numero.add(9);
+		numero.add(10);
+		
+		numero.remove(4);
+		System.out.println(numero);
+	
+	}
+}
---
+++ Lpaaula58.java
@@ -0,0 +1,10 @@
+package LPAlore;
+
+public class Lpaaula58 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
